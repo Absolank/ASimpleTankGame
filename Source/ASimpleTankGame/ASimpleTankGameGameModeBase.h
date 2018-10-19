b6b78f190ee56674a68dc641d260f8ade9ddf175
Ankit Kripal <ankitkripal@gmail.com>
2018-10-20 01:42:44 +0530

    Adding GameEngine Files

---
+++ Source/ASimpleTankGame/ASimpleTankGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ASimpleTankGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASIMPLETANKGAME_API AASimpleTankGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
